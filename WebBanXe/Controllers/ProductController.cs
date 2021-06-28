cff15db30fd61dc9fe048e007dc32bd6d69a895a
kai24112k <76855593+kai24112k@users.noreply.github.com>
2021-06-28 21:24:37 +0700

    Initial commit

---
+++ WebBanXe/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanXe.Model;
+
+namespace WebBanXe.Controllers
+{
+    public class ProductController : Controller
+    {
+        DBBanXeEntities db = new DBBanXeEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            //if (brand != null)
+            //{
+            //    List<PRODUCT> listProduct = new List<PRODUCT>();
+            //    listProduct = db.PRODUCTs.Where(p => p.BRAND.IdBrand == brand).ToList();
+            //    ViewBag.listProduct = listProduct; // lấy sản phẩm
+            //    ViewBag.title_product = db.BRANDs.Where(p => p.IdBrand == brand).FirstOrDefault(); //lấy tiêu đề
+            //}
+            //else
+            //{
+                List<PRODUCT> listProduct = new List<PRODUCT>();
+                listProduct = db.PRODUCTs.ToList();
+                ViewBag.listProduct = listProduct;// lấy sản phẩm
+                ViewBag.title_product = "Tất cả sản phẩm"; // lấy tiêu đề
+            //}
+            return View();
+        }
+    }
+}
