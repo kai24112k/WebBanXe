06fa8dcf659e77f56ccdd98486fa70106613f98e
dat1508 <datpham1508@gmail.com>
2021-07-04 19:56:44 +0700

    fix

--- WebBanXe/Public/js/front.js
+++ WebBanXe/Public/js/front.js
@@ -14,7 +14,7 @@ $(function () {
             $('header.nav-holder.make-sticky').removeClass('sticky');
             $('body').css('padding-top', '0');
         }
-    }); 
+    });
 
     // ------------------------------------------------------- //
     // Multi-level dropdown
@@ -90,17 +90,26 @@ $(function () {
     // ------------------------------------------------------- //
     // Customers Slider
     // ------------------------------------------------------ //
-    $(".customers").owlCarousel({
+    $(".brand").owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 800,
+        addClassActive: true,
+
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2
+                items: 1
             },
             600: {
-                items: 3
+                items: 2
             },
             1000: {
-                items: 6
+                items: 6,
+                loop: true
             }
         }
     });
@@ -137,10 +146,7 @@ $(function () {
         autoplay: true,
         smartSpeed: 1000,
         addClassActive: true,
-        navText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
+        navText: ["<img src='../img/left-arrow.png'>", "<img src='i../img/right-arrow.png'>"],
         responsiveClass: true,
         responsive: {
             0: {
@@ -171,7 +177,32 @@ $(function () {
     // ------------------------------------------------------- //
     // Project Caroudel
     // ------------------------------------------------------ //
-    $('.project').owl-carousel({
+    // $('.project').owlCarousel({
+    //     loop: true,
+    //     margin: 0,
+    //     dots: true,
+    //     nav: true,
+    //     autoplay: true,
+    //     smartSpeed: 1000,
+    //     addClassActive: true,
+    //     lazyload: true,
+    //     navText: ["<img src='img/left-arrow.png'>", "<img src='./img/right-arrow.png'>"],
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         600: {
+    //             items: 1
+    //         },
+    //         1000: {
+    //             items: 1,
+    //             loop: true
+    //         }
+    //     }
+    // });
+
+    $('.project').owlCarousel({
         loop: true,
         margin: 0,
         dots: true,
@@ -180,7 +211,7 @@ $(function () {
         smartSpeed: 1000,
         addClassActive: true,
         lazyload: true,
-     
+        navText: ["<img src='./Public/img/left-arrow.png'>", "<img src='./Public/img/right-arrow.png'>"],
         responsiveClass: true,
         responsive: {
             0: {
@@ -195,8 +226,30 @@ $(function () {
             }
         }
     });
-
-
+    $('.product').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        addClassActive: true,
+        lazyload: true,
+        navText: ["<img src='/Public/img/left-arrow.png'>", "<img src='/Public/img/right-arrow.png'>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1,
+                loop: true
+            }
+        }
+    });
     // ------------------------------------------------------- //
     // jQuery Counter Up
     // ------------------------------------------------------ //
@@ -288,5 +341,26 @@ $(function () {
             });
         }
     });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        animateOut: 'fadeOut',
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
+        nav: true,
+        navText: ["<img src='./img/left-arrow.png'>", "<img src='./img/right-arrow.png'>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
 
 });
