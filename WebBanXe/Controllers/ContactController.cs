a592069d802cf63c12758e040cc32f5379d64695
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2021-07-05 23:48:06 +0700

    update Login, SignUp and Contact

---
+++ WebBanXe/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanXe.Model;
+
+namespace WebBanXe.Controllers
+{
+    public class ContactController : Controller
+    {
+        DBBanXeEntities db = new DBBanXeEntities();
+        // GET: Contact
+        [HttpPost]
+        public ActionResult ContactPage(FormCollection contactForm)
+        {
+            CONTACT contact = new CONTACT();
+
+
+           
+            var title = contactForm["title"];
+            var email = contactForm["email"];
+            var content = contactForm["content"];
+           
+            var detail = contactForm["noidung"];
+            contact.Title = title;
+            contact.Email = email;
+            contact.Content = content;
+           
+            db.CONTACTs.Add(contact);
+            db.SaveChanges();
+            ViewBag.Notice = "<div class='alert alert-success text-center text-dark' role='alert'>Gửi liên hệ thành công</div>";
+            return View();
+        }
+
+        public ActionResult ContactPage()
+        {
+            return View();
+           
+        }
+    }
+}
---
+++ WebBanXe/Controllers/CustomerController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanXe.Model;
+
+namespace WebBanXe.Controllers
+{
+    public class CustomerController : Controller
+    {
+        DBBanXeEntities db = new DBBanXeEntities();
+        // GET: Customer
+        [HttpGet]
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection frm)
+        {
+            var userName = frm["userName"];
+            var password = frm["password"];
+
+            if (String.IsNullOrEmpty(userName))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+
+            }
+            else if (String.IsNullOrEmpty(password))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                USER user = db.USERs.SingleOrDefault(u => u.Username == userName && u.Password == password && u.IdRole == 3);
+                if (user != null)
+                {
+                    Session["userID"] = user.IdUser;
+                    Session["fullName"] = user.FullName;
+                    ViewBag.Notice = "<div class='alert alert-success text-center text-dark' role='alert'>Đăng nhập thành công</div>";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+            
+                    ViewBag.ThongBao = "Đăng nhập không thành công";
+            }
+            return View();
+
+        }
+        public ActionResult Logout()
+        {
+            Session["userID"] = null;
+            Session["fullName"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(FormCollection collection)
+        {
+            USER user = new USER();
+            user.FullName = collection["HoTen"];
+            user.Email = collection["Email"];
+            user.Username = collection["TaiKhoan"];
+            user.Password = collection["MatKhau"];
+            user.Address = collection["DiaChi"];
+            user.Phone = collection["Sdt"];
+            user.DayCreate = DateTime.Now;
+           
+            if (String.IsNullOrEmpty(user.FullName))
+            {
+                ViewData["Loi1"] = "Họ tên không được để trống";
+            }
+            else if (String.IsNullOrEmpty(user.Email))
+            {
+                ViewData["Loi2"] = "Phải nhập email";
+            }
+            else if (String.IsNullOrEmpty(user.Username))
+            {
+                ViewData["Loi3"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(user.Password))
+            {
+                ViewData["Loi4"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(user.Address))
+            {
+                ViewData["Loi5"] = "Địa chỉ không được để trống";
+            }
+            else if (String.IsNullOrEmpty(user.Phone))
+            {
+                ViewData["Loi6"] = "Phải nhập số điện thoại";
+            }
+            
+            else
+            {
+                if (collection["XacNhanMatKhau"] == collection["MatKhau"])
+                {
+                    user.IdRole = 3;
+                    db.USERs.Add(user);
+                    db.SaveChanges();
+                    ViewBag.Notice = "<div class='alert alert-success text-center text-dark' role='alert'>Gửi liên hệ thành công</div>";
+                    return RedirectToAction("Login", "Customer");
+
+                }
+                else
+                {
+                    ViewData["Loi4"] = " Vui lòng xác nhận lại mật khẩu";
+                }
+
+                return this.SignUp();
+            }
+            return this.View();
+        }
+
+       
+    }
+}
